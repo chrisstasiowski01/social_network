64467e598fa490c5d5eae83b30dd2a5db2a16eab
Chris Stasiowski <cstasiowski@umail.ucsb.edu>
2021-05-24 13:10:23 -0700

    left/right rotate

--- redBlack.cpp
+++ redBlack.cpp
@@ -110,3 +110,41 @@ void redBlack::fixInsert(Node* k){
   }
   root->color = 0;
 }
+
+void redBlack::leftRotate(Node* x){
+  Node* y = x->right;
+  x->right = y->left;
+  if(y->left != TNULL){
+    y->left->parent = x;
+  }
+  y->parent = x->parent;
+  if(x->parent == NULL){
+    root = y;
+  }else if(x == x->parent->left){
+    x->parent->left = y;
+  }else{
+    x->parent->right = y;
+  }
+
+  y->left = x;
+  x->parent = y;
+}
+
+void redBlack::rightRotate(Node* x){
+  Node* y = x->left;
+  x->left = y->right;
+  if(y->right != TNULL){
+    y->right->parent = x;
+  }
+  y->parent = x->parent;
+  if(x->parent == NULL){
+    root = y;
+  }else if(x == x->parent->left){
+    x->parent->left = y;
+  }else{
+    x->parent->right = y;
+  }
+
+  y->right = x;
+  x->parent = y;
+}
