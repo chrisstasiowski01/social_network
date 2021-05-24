a501415ef1eadb2a907cf12e47197dd1b12bfce3
Chris Stasiowski <cstasiowski@umail.ucsb.edu>
2021-05-24 13:04:46 -0700

    insert helper

--- redBlack.cpp
+++ redBlack.cpp
@@ -66,3 +66,47 @@ void redBlack::insert(string n, int i){
   // else, call helper function to rotate
   fixInsert(node);
 }
+
+
+void redBlack::fixInsert(Node* k){
+  Node* n;
+  while(k->parent->color == 1){
+    if(k->parent = k->parent->parent->right){
+      n = k->parent->parent->left;
+      if(n->color == 1){
+        n->color = 0;
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        k = k->parent->parent;
+      }else{
+        if(k == k->parent->left){
+          k = k->parent;
+          rightRotate(k);
+        }
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        leftRotate(k->parent->parent);
+      }
+    }else{
+      n = k->parent->parent->right;
+      if(n->color == 1){
+        n->color = 0;
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        k = k->parent->parent;
+      }else{
+        if(k == k->parent->right){
+          k = k->parent;
+          leftRotate(k);
+        }
+        k->parent->color = 0;
+        k->parent->parent->color = 1;
+        rightRotate(k->parent->parent);
+      }
+    }
+    if(k == root){
+      break;
+    }
+  }
+  root->color = 0;
+}
